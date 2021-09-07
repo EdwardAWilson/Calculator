6f1889c9d988780e3e479a250ee1a6b21424fbd7
Edward Wilson <edward.wil004@gmail.com>
2021-09-07 00:20:18 -0500

    Added input reading function

--- calculator.c
+++ calculator.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main()
+#define MAX_LENGTH 80
+
+int main(int argc, char *argv[])
 {
+	char calculateString[MAX_LENGTH];
+
+	printf("Enter equation to be calculated: ");
+	scanf("%s", calculateString);
+
+	long firstNumber = strtol(strtok(calculateString, " "), NULL, 10);
+	char* operator = strtok(calculateString, " ");
+	long secondNumber = strtol(strtok(calculateString, " "), NULL, 10);
+
+	if (!(firstNumber && operator && secondNumber))
+	{
+		printf("Error, please input full equation");
+		return 0;
+	}
 }
