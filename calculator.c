d615b3cfcbc05c02e1bd1dfec9a61bb9098cfcb1
Edward Wilson <edward.wil004@gmail.com>
2021-09-07 11:54:24 -0500

    Continuing to work on calculator

--- calculator.c
+++ calculator.c
@@ -10,14 +10,35 @@ int main(int argc, char *argv[])
 
 	printf("Enter equation to be calculated: ");
 	scanf("%s", calculateString);
-
+	
 	long firstNumber = strtol(strtok(calculateString, " "), NULL, 10);
-	char* operator = strtok(calculateString, " ");
-	long secondNumber = strtol(strtok(calculateString, " "), NULL, 10);
+	char operator = strtok(NULL, " ")[0];
+	long secondNumber = strtol(strtok(NULL, " "), NULL, 10);
 
 	if (!(firstNumber && operator && secondNumber))
 	{
 		printf("Error, please input full equation");
 		return 0;
 	}
-}
+
+	long ans = 0;
+
+	switch(operator)
+	{
+		case '+':
+			ans = firstNumber + secondNumber;
+			break;
+		case '-':
+			ans = firstNumber - secondNumber;
+			break;
+		case '*':
+		case 'x':
+			ans = firstNumber * secondNumber;
+			break;
+		case '/':
+			ans = firstNumber / secondNumber;
+			break;
+	}
+
+	printf("Your answer is %ld\n", ans);
+}			
